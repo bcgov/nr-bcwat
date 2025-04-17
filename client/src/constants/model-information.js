875065550d14faca34ed5137092796eeb0c12dac
Max Douglas <max@foundryspatial.com>
2025-04-17 11:53:04 -0700

    Add notes content

---
+++ client/src/constants/model-information.js
@@ -0,0 +1,19 @@
+// Foundry Spatial hydrology model info (for notes/references)
+export const FS_HYDROLOGY_MODEL = {
+    numWatersheds: '45',
+    meanError: '5.5',
+    medianError: '3.7',
+    meanAbsoluteError: '16.1',
+    watershedsWithin20Percent: '77.8',
+    cwnaDriverYears: '1961-1990',
+    cwnaVersion: '4.62',
+};
+
+export const CLIMATE_CHART = {
+    cwnaDriverYears: '1971-2000',
+    cwnaVersion: '4.72',
+    futureDriverYears: '2041-2070',
+    futureScenario1: 'HadGEM A1B',
+    futureScenario2: 'CGCM3 A2',
+    futureScenario3: 'HadCM3 B1',
+};
