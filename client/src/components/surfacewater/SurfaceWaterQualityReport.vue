22d0bc5aca676b4dd9fdc6a4b50672104b0ede28
Steven Sonvisen <sonvisenss@gmail.com>
2025-05-12 16:27:29 -0700

    made some columns sticky in surface water quality table

--- client/src/components/surfacewater/SurfaceWaterQualityReport.vue
+++ client/src/components/surfacewater/SurfaceWaterQualityReport.vue
@@ -50,61 +50,70 @@
             <div class="data-license cursor-pointer">Data License</div>
         </div>
         <q-tab-panels v-model="viewPage">
-            <q-tab-panel name="surfaceWaterQuality">
-                <table class="surface-water-quality-table">
-                    <!-- rows of data in the response array -->
-                    <tr>
-                        <th>
-                            Parameter
-                        </th>
-                        <th>
-                            Chart
-                        </th>
-                        <th 
-                            v-if="chemistry.sparkline"
-                            :colspan="Math.max(...chemistry.sparkline.map(el => el.data.length))"
-                        >
-                            Entries
-                        </th>
-                    </tr>
-                    <tr 
-                        v-for="(param, idx) in chemistry.sparkline"
-                        :name="idx"
-                    >
-                        <td>
-                            {{ param.title }} ({{ param.units }})
-                        </td>
-                        <td>
-                            <div class="mini-chart">
-                                <div class="mini-chart-overlay">
-                                    <q-btn
-                                        class="chart-expand"
-                                        icon="add"
-                                        dense
-                                        @click="() => selectChart(param)"
-                                    />
-                                </div>
-                                <SurfaceWaterQualityMiniChart 
-                                    :selected-point="props.activePoint"
-                                    :chart-data="param.data"
-                                    :chart-id="`surface-quality-chart-mini-${param.paramId}`"
-                                />
-                            </div>
-                        </td>
-                        <td 
-                            v-for="datapoint in chemistry.sparkline[idx].data"
-                            class="table-cell"
-                        >
-                            <div class="text-bold">
-                                {{ formatHeaderDate(datapoint.d) }}
-                            </div>
-                            <q-separator />
-                            <div>
-                                {{ datapoint.v || 'No Data' }} 
-                            </div>
-                        </td>
-                    </tr>
-                </table>
+            <q-tab-panel 
+                class="water-quality-panel"
+                name="surfaceWaterQuality"
+            >
+                <div class="page-header text-h4">
+                    Surface Water Quality
+                </div>
+                <div  class="surface-water-quality-table">
+                    <table>
+                        <tbody>
+                            <!-- rows of data in the response array -->
+                            <tr>
+                                <th class="header-text">
+                                    Parameter
+                                </th>
+                                <th />
+                                <th 
+                                    v-if="chemistry.sparkline"
+                                    class="header-text"
+                                    :colspan="Math.max(...chemistry.sparkline.map(el => el.data.length))"
+                                >
+                                    Entries
+                                </th>
+                            </tr>
+                            <tr 
+                                v-for="(param, idx) in chemistry.sparkline"
+                                :name="idx"
+                            >
+                                <td>
+                                    {{ param.title }} ({{ param.units }})
+                                </td>
+                                <td>
+                                    <div class="mini-chart">
+                                        <div class="mini-chart-overlay">
+                                            <q-btn
+                                                class="chart-expand"
+                                                icon="add"
+                                                dense
+                                                @click="() => selectChart(param)"
+                                            />
+                                        </div>
+                                        <SurfaceWaterQualityMiniChart 
+                                            :selected-point="props.activePoint"
+                                            :chart-data="param.data"
+                                            :chart-id="`surface-quality-chart-mini-${param.paramId}`"
+                                        />
+                                    </div>
+                                </td>
+                                <td 
+                                    v-for="datapoint in chemistry.sparkline[idx].data"
+                                    class="table-cell"
+                                >
+                                    <div class="text-bold">
+                                        {{ formatHeaderDate(datapoint.d) }}
+                                    </div>
+                                    <q-separator />
+                                    <div>
+                                        {{ datapoint.v || 'No Data' }} 
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </q-tab-panel>
         </q-tab-panels>
 
@@ -190,6 +199,10 @@ const formatHeaderDate = (date) => {
     text-decoration: underline;
 }
 
+.page-header {
+    color: #777
+}
+
 .about {
     cursor: pointer;
 }
@@ -200,13 +213,57 @@ const formatHeaderDate = (date) => {
     }
 }
 
+.water-quality-panel {
+    overflow-x: hidden;
+}
+
 .surface-water-quality-table {
-    overflow-x: scroll;
-    
+    overflow: auto;
+
+    table {
+        th:first-child{
+            background-color: white;
+            position: sticky;
+            position: -webkit-sticky;
+            left: 0;
+            z-index: 2;
+            top: 0;
+        }
+        th:nth-child(2){
+            background-color: white;
+            position: sticky;
+            position: -webkit-sticky;
+            left: 100px;
+            z-index: 2;
+            top: 0;
+        }
+        td:first-child {
+            background-color: white;
+            position: sticky;
+            position: -webkit-sticky;
+            left: 0;
+            z-index: 2;
+            top: 0;
+        }
+        td:nth-child(2) {
+            background-color: white;
+            position: sticky;
+            position: -webkit-sticky;
+            left: 100px;
+            z-index: 2;
+            top: 0;
+        }
+    }
+
     .header-text {
         color: grey;
     }
-    th, td {
+    th {
+        padding: 8px;
+        text-align: left;
+    }
+
+    td {
         padding: 8px;
         text-align: left;
         border-bottom: 1px solid #ddd;
