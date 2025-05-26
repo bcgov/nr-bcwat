cd97fe93f407aa1fe37518d0353fb9f7a46baa09
Steven Sonvisen <sonvisenss@gmail.com>
2025-05-26 15:50:41 -0700

    added climate page test

--- client/cypress/component/climate/ClimatePage.cy.js
+++ client/cypress/component/climate/ClimatePage.cy.js
@@ -1,5 +1,19 @@
+import ClimatePage from "@/components/climate/ClimatePage.vue";
+
 describe('<ClimatePage />', () => {
     it('mounts and renders components', () => {
-        
+        cy.mount(ClimatePage);
+        cy.get('.search-entry').should('exist');
+        cy.get('.map-filters-container').should('exist');
+    });
+    it('opens and closes report', () => {
+        cy.mount(ClimatePage);
+        cy.get('.q-virtual-scroll__content')
+            .children()
+            .first()
+            .click();
+        cy.get('.q-btn > span > span').contains('View More').click();
+        cy.get('.chart-area').should('exist').and('be.visible');
+        cy.get('.q-btn > span > span.block').contains('Back to Map').click();
     });
 });
