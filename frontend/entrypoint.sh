5787073973ff199888ed9a3e472358f4daa4a7fd
LiamF-FS <liam@foundryspatial.com>
2025-05-28 15:49:36 -0700

    Define Variables to be updated within env.js

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -7,7 +7,7 @@ echo "Injecting runtime environment variables..."
 envsubst '$LOG_LEVEL $BACKEND_URL' < /etc/nginx/templates/nginx.conf.template > /tmp/nginx.conf
 
 # Produce Env.js at run time
-envsubst < /app/env.js.template > /app/env.js
+envsubst '$VITE_BASE_API_URL $VITE_APP_MAPBOX_TOKEN' < /app/env.js.template > /app/env.js
 
 # Start NGINX with custom config path
 exec nginx -g "daemon off;" -c /tmp/nginx.conf
