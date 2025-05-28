31e9f233a8f7f98b63a5803f8e1ebb3d5b2ecbf9
LiamF-FS <liam@foundryspatial.com>
2025-05-28 16:39:08 -0700

    Fixed in Docker Compose, Testing Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,36 +1,31 @@
 import { fileURLToPath, URL } from "node:url";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default ({ mode }) => {
-  // Load VITE_* vars from .env files into process.env
-  loadEnv(mode, process.cwd(), "");
-
-  return defineConfig({
+export default defineConfig({
     plugins: [
-      vue({
-        template: {
-          transformAssetUrls,
-        },
-      }),
-      quasar({
-        sassVariables: "@/assets/quasar-variables.sass",
-      }),
+        vue({
+            template: {
+                transformAssetUrls,
+            },
+        }),
+        quasar({
+            sassVariables: "@/assets/quasar-variables.sass",
+        }),
     ],
     resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
     css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@import "@/assets/main.scss";`,
-          api: "modern-compiler",
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/assets/main.scss";`,
+                api: "modern-compiler",
+            },
         },
-      },
     },
-  });
-};
+});
