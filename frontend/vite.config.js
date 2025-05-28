18c7afd4f1ec12ca8e914a4b9f04d19e12810586
LiamF-FS <liam@foundryspatial.com>
2025-05-28 16:05:13 -0700

    loadEnv in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,36 @@
 import { fileURLToPath, URL } from "node:url";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default ({ mode }) => {
+  // Load VITE_* vars from .env files into process.env
+  loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
     plugins: [
-        vue({
-            template: {
-                transformAssetUrls,
-            },
-        }),
-        quasar({
-            sassVariables: "@/assets/quasar-variables.sass",
-        }),
+      vue({
+        template: {
+          transformAssetUrls,
+        },
+      }),
+      quasar({
+        sassVariables: "@/assets/quasar-variables.sass",
+      }),
     ],
     resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
     css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@import "@/assets/main.scss";`,
-                api: "modern-compiler",
-            },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/main.scss";`,
+          api: "modern-compiler",
         },
+      },
     },
-});
+  });
+};
