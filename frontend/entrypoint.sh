b3062350bdec6a9de09fb094c85a10c93a35748e
LiamF-FS <liam@foundryspatial.com>
2025-05-28 15:35:11 -0700

    Produce tmp nginx.conf, use in entrypoint

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,8 +3,11 @@ set -e
 
 echo "Injecting runtime environment variables..."
 
-# Substitute env.js
+# Render full config
+envsubst '$LOG_LEVEL $BACKEND_URL' < /etc/nginx/templates/nginx.conf.template > /tmp/nginx.conf
+
+# Produce Env.js at run time
 envsubst < /app/env.js.template > /app/env.js
 
-# Let the official docker-entrypoint.sh finish the rest (including nginx.conf.template substitution)
-exec /docker-entrypoint.sh nginx -g "daemon off;"
+# Start NGINX with custom config path
+exec nginx -g "daemon off;" -c /tmp/nginx.conf
