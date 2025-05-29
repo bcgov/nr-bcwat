5dac250ea5f1965e656110d372ae412133b03a7c
LiamF-FS <liam@foundryspatial.com>
2025-05-29 10:23:07 -0700

    Testing with Root User Again

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "Injecting runtime environment variables..."
 
-# Render full config
-envsubst '$LOG_LEVEL $BACKEND_URL' < /etc/nginx/templates/nginx.conf.template > /tmp/nginx.conf
+# # Render full config
+envsubst '$LOG_LEVEL $BACKEND_URL' < /etc/nginx/templates/default.conf.template > /etc/nginx/nginx.conf
 
 # Produce Env.js at run time
 envsubst '$VITE_BASE_API_URL $VITE_APP_MAPBOX_TOKEN' < /app/env.js.template > /app/env.js
 
 # Start NGINX with custom config path
-exec nginx -g "daemon off;" -c /tmp/nginx.conf
+exec nginx -g "daemon off;"
