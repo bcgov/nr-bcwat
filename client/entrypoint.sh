20826d58b971974567b01e413a5ee07eedea3796
LiamF-FS <liam@foundryspatial.com>
2025-05-29 16:52:34 -0700

    indents, remove outdated comments

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -6,5 +6,4 @@ echo "Injecting runtime environment variables..."
 # Produce Env.js at run time
 envsubst '$VITE_BASE_API_URL $VITE_APP_MAPBOX_TOKEN' < /app/env.js.template > /app/env.js
 
-# Start NGINX with custom config path
 exec nginx -g "daemon off;"
