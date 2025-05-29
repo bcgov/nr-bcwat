921c937d70aba220c0d1ada103227cd9ef5d1d44
LiamF-FS <liam@foundryspatial.com>
2025-05-29 10:58:32 -0700

    Dynamically Produce Nginx.conf via Configmap, helm chart

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,9 +3,6 @@ set -e
 
 echo "Injecting runtime environment variables..."
 
-# # Render full config
-envsubst '$LOG_LEVEL $BACKEND_URL' < /etc/nginx/templates/default.conf.template > /etc/nginx/nginx.conf
-
 # Produce Env.js at run time
 envsubst '$VITE_BASE_API_URL $VITE_APP_MAPBOX_TOKEN' < /app/env.js.template > /app/env.js
 
