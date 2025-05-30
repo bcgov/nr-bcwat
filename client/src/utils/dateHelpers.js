aac4f2dc6f0c50e21a59b9619dce5628f036f6d0
Steven Sonvisen <sonvisenss@gmail.com>
2025-05-30 15:49:05 -0700

    additional Allocations test

--- client/src/utils/dateHelpers.js
+++ client/src/utils/dateHelpers.js
@@ -12,3 +12,24 @@ export const monthAbbrList = [
     "Nov",
     "Dec",
 ];
+
+export const formatDate = (date, format, separator) => {
+    const dateParts = format.split(' ');
+    const dateString = dateParts.map(part => {
+        if(part === 'mm'){
+            const monthNum = date.getUTCMonth() + 1;
+            return monthNum < 10 ? `0${monthNum}` : monthNum;
+        }
+        if(part === 'mmm'){
+            const monthIdx = date.getUTCMonth();
+            return monthAbbrList[monthIdx];
+        }
+        if(part === 'dd'){
+            return date.getUTCDate();
+        }
+        if(part === 'yyyy'){
+            return date.getUTCFullYear();
+        }
+    }).join(separator)
+    return dateString;
+}
