b9b1be99efe27e3912d18d8beea42b7a200b690a
Steven Sonvisen <sonvisenss@gmail.com>
2025-06-06 11:27:40 -0700

    test date helper update to use UTC

--- client/src/utils/dateHelpers.js
+++ client/src/utils/dateHelpers.js
@@ -17,19 +17,19 @@ export const formatDate = (date, format, separator) => {
     const dateParts = format.split(' ');
     const dateString = dateParts.map(part => {
         if(part === 'mm'){
-            const monthNum = date.getMonth() + 1;
+            const monthNum = date.getUTCMonth() + 1;
             return monthNum < 10 ? `0${monthNum}` : monthNum;
         }
         if(part === 'mmm'){
-            const monthIdx = date.getMonth();
+            const monthIdx = date.getUTCMonth();
             return monthAbbrList[monthIdx];
         }
         if(part === 'dd'){
-            const day = date.getDate();
+            const day = date.getUTCDate();
             return day < 10 ? `0${day}` : day;
         }
         if(part === 'yyyy'){
-            return date.getFullYear();
+            return date.getUTCFullYear();
         }
     }).join(separator)
     return dateString;
