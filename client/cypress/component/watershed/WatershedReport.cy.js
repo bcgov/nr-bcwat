7f9d72dc89dfde0591922e9598bb09e541521fa3
Steven Sonvisen <sonvisenss@gmail.com>
2025-06-05 17:08:52 -0700

    added remaining report tests and in-progress watershed report overall component

---
+++ client/cypress/component/watershed/WatershedReport.cy.js
@@ -0,0 +1,17 @@
+import WatershedReport from '@/components/watershed/WatershedReport.vue';
+import watershedReport from '@/constants/watershedReport.json';
+
+const reportContent  = watershedReport;
+
+describe('<WatershedReport />', () => {
+    it('renders report contents', () => {
+        cy.mount(WatershedReport, {
+            props: {
+                reportOpen: true,
+                reportContent,
+                clickedPoint: { lng: -122, lat: 50 }
+            }
+        });
+        cy.get('h1')
+    });
+});
