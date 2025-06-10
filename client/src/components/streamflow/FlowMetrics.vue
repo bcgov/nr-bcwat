96783a0392e59cdd9094e695492afc2d144cd2ce
Steven Sonvisen <sonvisenss@gmail.com>
2025-06-10 09:27:59 -0700

    updated FlowMetrics.vue component to set table columns and rows

--- client/src/components/streamflow/FlowMetrics.vue
+++ client/src/components/streamflow/FlowMetrics.vue
@@ -35,12 +35,20 @@ onMounted(async () => {
 const formatTableData = (data) => {
     if(data.length > 0){
         tableCols.value = [
-            { name: 'Parameter' },
-        ]
-
+            { name: 'Parameter', field: 'Parameter', label: 'Parameter' },
+            { name: '200', field: '200', label: '200' },
+            { name: '100', field: '100', label: '100' },
+            { name: '50', field: '50', label: '50' },
+            { name: '25', field: '25', label: '25' },
+            { name: '20', field: '20', label: '20' },
+            { name: '10', field: '10', label: '10' },
+            { name: '5', field: '5', label: '5' },
+            { name: '2', field: '2', label: '2' },
+            { name: '1.01', field: '1.01', label: '1.01' },
+            { name: '1', field: '1', label: '1' },
+            { name: 'Years of data', field: 'Years of data', label: 'Years of data' },
+        ];
         tableRows.value = data;
     }
-
-    console.log(tableCols.value)
 };
 </script>
