2122dac7838206758822454f5cbf76b452731e78
Kashike-Umemura <kashikeumemura@gmail.com>
2025-06-24 13:19:13 -0700

    station fix and clean up

--- airflow/etl_pipelines/scrapers/QuarterlyPipeline/quarterly/hydat_import.py
+++ airflow/etl_pipelines/scrapers/QuarterlyPipeline/quarterly/hydat_import.py
@@ -19,6 +19,7 @@ import polars as pl
 import polars.selectors as cs
 import polars_st as st
 import os
+import glob
 import pendulum
 import zipfile
 from bs4 import BeautifulSoup
@@ -684,3 +685,22 @@ class HydatPipeline(StationObservationPipeline):
             logger.error(f"Updating import date for Hydat failed!")
             raise RuntimeError(f"Updating import date for Hydat failed! Error: {e}")
 
+    def clean_up(self):
+        """
+        Method to clean up the downloaded files that is not required anymore.
+
+        Args:
+            None
+
+        Output:
+            None
+        """
+
+        logger.info("Cleaning up the downloaded Hydat.sqlite3 database file")
+
+        files = glob.glob(self.file_path)
+
+        for file in files:
+            os.remove(file)
+
+        logger.info(f"Finished Cleaning up")
