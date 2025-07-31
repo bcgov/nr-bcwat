612e77176efa2c3d9cd2c2b8768a35212e5b834a
LiamF-FS <liam@foundryspatial.com>
2025-07-31 11:05:49 -0700

    Update Response of WFI/WLS search

--- backend/queries/watershed/get_watershed_by_search_term.py
+++ backend/queries/watershed/get_watershed_by_search_term.py
@@ -1,10 +1,11 @@
 get_watershed_by_search_term_query = """
     SELECT
-        watershed_feature_id,
+        watershed_feature_id as id,
         area_m2,
         longitude,
         latitude,
-        COALESCE(gnis_name, 'Unnamed Basin')
+        COALESCE(gnis_name, 'Unnamed Basin'),
+        ST_AsGeoJson(up.upstream_geom_4326_z12)::json as geojson
     FROM
         bcwat_ws.ws_geom_all_report
     WHERE
--- backend/queries/watershed/get_watershed_licences_by_search_term.py
+++ backend/queries/watershed/get_watershed_licences_by_search_term.py
@@ -1,16 +1,12 @@
 get_watershed_licences_by_search_term_query = """
     SELECT
         wls_id,
-        branding_organization,
         licensee,
         licence_no,
         ann_adjust,
-        industry_activity,
-        lic_status,
-        water_allocation_type,
-        purpose,
-        purpose_groups,
-        licence_term
+        licence_term,
+        longitude,
+        latitude
     FROM
         bcwat_lic.licence_wls_map
     WHERE
