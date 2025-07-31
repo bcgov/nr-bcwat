da0f78d2daed456c5b58ac808776cc06d30cc95f
LiamF-FS <liam@foundryspatial.com>
2025-07-31 15:19:29 -0700

    Remove OrderBy/Redundant Limit

--- backend/queries/watershed/get_watershed_by_search_term.py
+++ backend/queries/watershed/get_watershed_by_search_term.py
@@ -10,7 +10,4 @@ get_watershed_by_search_term_query = """
         bcwat_ws.ws_geom_all_report
     WHERE
         watershed_feature_id = %(watershed_feature_id)s
-    ORDER BY
-        watershed_feature_id
-    LIMIT 10;
 """
