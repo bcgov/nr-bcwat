c26aafe44515ba2f570307a711cbad3c95183bb8
LiamF-FS <liam@foundryspatial.com>
2025-08-06 11:41:09 -0700

    Initialize PGBouncer on Openshift Dev

--- database_initialization/queries/post_import_queries.py
+++ database_initialization/queries/post_import_queries.py
@@ -2,6 +2,32 @@ post_import_query = '''
 
 ALTER TABLE IF EXISTS "bcwat_obs"."station" DROP COLUMN IF EXISTS "old_station_id";
 
+ALTER DEFAULT PRIVILEGES IN SCHEMA bcwat_lic GRANT SELECT ON TABLES TO "bcwat-api-read-only";
+ALTER DEFAULT PRIVILEGES IN SCHEMA bcwat_obs GRANT SELECT ON TABLES TO "bcwat-api-read-only";
+ALTER DEFAULT PRIVILEGES IN SCHEMA bcwat_ws GRANT SELECT ON TABLES TO "bcwat-api-read-only";
+
+GRANT USAGE ON SCHEMA bcwat_lic TO "bcwat-api-read-only";
+GRANT USAGE ON SCHEMA bcwat_obs TO "bcwat-api-read-only";
+GRANT USAGE ON SCHEMA bcwat_ws TO "bcwat-api-read-only";
+
+GRANT SELECT ON ALL TABLES IN SCHEMA bcwat_lic TO "bcwat-api-read-only";
+GRANT SELECT ON ALL TABLES IN SCHEMA bcwat_obs TO "bcwat-api-read-only";
+GRANT SELECT ON ALL TABLES IN SCHEMA bcwat_ws TO "bcwat-api-read-only";
+
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA bcwat_lic TO "bcwat-api-read-only";
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA bcwat_lic GRANT SELECT, UPDATE, DELETE, TRUNCATE, INSERT ON TABLES TO "bcwat-airflow-read-write";
+ALTER DEFAULT PRIVILEGES IN SCHEMA bcwat_obs GRANT SELECT, UPDATE, DELETE, TRUNCATE, INSERT ON TABLES TO "bcwat-airflow-read-write";
+ALTER DEFAULT PRIVILEGES IN SCHEMA bcwat_ws GRANT SELECT, UPDATE, DELETE, TRUNCATE, INSERT ON TABLES TO "bcwat-airflow-read-write";
+
+GRANT USAGE ON SCHEMA bcwat_lic TO "bcwat-airflow-read-write";
+GRANT USAGE ON SCHEMA bcwat_obs TO "bcwat-airflow-read-write";
+GRANT USAGE ON SCHEMA bcwat_ws TO "bcwat-airflow-read-write";
+
+GRANT SELECT, UPDATE, DELETE, TRUNCATE, INSERT ON ALL TABLES IN SCHEMA bcwat_lic TO "bcwat-airflow-read-write";
+GRANT SELECT, UPDATE, DELETE, TRUNCATE, INSERT ON ALL TABLES IN SCHEMA bcwat_obs TO "bcwat-airflow-read-write";
+GRANT SELECT, UPDATE, DELETE, TRUNCATE, INSERT ON ALL TABLES IN SCHEMA bcwat_ws TO "bcwat-airflow-read-write";
+
 -- TRIGGERS --
     CREATE OR REPLACE FUNCTION bcwat_obs.fill_geom_point() RETURNS TRIGGER AS $$
     BEGIN
@@ -2756,5 +2782,4 @@ AS $BODY$
 	END
 
 $BODY$;
-
 '''
