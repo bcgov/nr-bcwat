f8b5ea3021a2ee26ea092790a91bbb6f444d70e1
Nathan E <nenerson@foundryspatial.com>
2025-08-15 16:55:57 -0700

    query formatting

--- backend/queries/watershed/get_watershed_fwa_by_id.py
+++ backend/queries/watershed/get_watershed_fwa_by_id.py
@@ -1,8 +1,8 @@
 get_fwa_query = """
-SELECT
-    fwa_watershed_code AS fwa_watershed_code
-FROM
-    bcwat_ws.ws_geom_all_report
-WHERE
-    watershed_feature_id = %(watershed_feature_id)s;
+    SELECT
+        fwa_watershed_code AS fwa_watershed_code
+    FROM
+        bcwat_ws.ws_geom_all_report
+    WHERE
+        watershed_feature_id = %(watershed_feature_id)s;
 """
