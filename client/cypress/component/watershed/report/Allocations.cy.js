2fc9a207db592e8a3dee1971e1e76b0d7d1fa639
Max Douglas <max@foundryspatial.com>
2025-08-22 10:45:14 -0700

    fix test

--- client/cypress/component/watershed/report/Allocations.cy.js
+++ client/cypress/component/watershed/report/Allocations.cy.js
@@ -21,7 +21,7 @@ reportData.allocations.push({
     priority_date: new Date(),
     expiry_date: new Date(),
     lic_status_date: new Date(),
-    qty_display: "1234",
+    old_ann_adjust: 1234,
     qty_flag: "Test",
     lic_type: "sw-lic",
     lic_status: "CURRENT",
@@ -58,7 +58,7 @@ describe('<Allocations />', () => {
         cy.get('.q-table > tbody > tr > td:nth-child(4) > p:nth-child(2)').should('contain', formatDate(allocationRow.priority_date, 'dd mmm yyyy', ' '));
         cy.get('.q-table > tbody > tr > td:nth-child(4) > p:nth-child(3)').should('contain', formatDate(allocationRow.expiry_date, 'dd mmm yyyy', ' '));
         cy.get('.q-table > tbody > tr > td:nth-child(4) > p:nth-child(4)').should('contain', formatDate(allocationRow.lic_status_date, 'dd mmm yyyy', ' '));
-        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', allocationRow.qty_display);
+        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', "1,234.0");
         cy.get('.q-table > tbody > tr > td:nth-child(6)').should('contain', allocationRow.qty_flag);
         cy.get('.q-table > tbody > tr > td:nth-child(7) > div').should('have.class', allocationRow.lic_type);
         cy.get('.q-table > tbody > tr > td:nth-child(7) > div').should('contain', allocationRow.lic_type);
@@ -72,24 +72,24 @@ describe('<Allocations />', () => {
         });
         const allocationRow = reportData.allocations[0];
         // check value exists before filtering
-        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', allocationRow.qty_display);
+        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', "1,234.0");
         cy.get('.mdi-filter').click()
         cy.get('.q-checkbox__label').contains('Surface Water').click()
         cy.get('.q-table > tbody > tr > td:nth-child(5)').should('not.exist');
         cy.get('.q-checkbox__label').contains('Surface Water').click()
-        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', allocationRow.qty_display);
+        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', "1,234.0");
         cy.get('.q-checkbox__label').contains('Application').click()
         cy.get('.q-table > tbody > tr > td:nth-child(5)').should('not.exist');
         cy.get('.q-checkbox__label').contains('Application').click();
         cy.get('.q-checkbox__label').contains('Agriculture').click();
         cy.get('.q-table > tbody > tr > td:nth-child(5)').should('not.exist');
         cy.get('.q-checkbox__label').contains('Agriculture').click();
-        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', allocationRow.qty_display);
+        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', "1,234.0");
         cy.get('input[placeholder="Text Search"]').type('TESTING')
         cy.get('.q-table > tbody > tr > td:nth-child(5)').should('not.exist');
         cy.get('input[placeholder="Text Search"]').clear()
         cy.get('input[placeholder="Text Search"]').type('Cypress')
-        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', allocationRow.qty_display);
+        cy.get('.q-table > tbody > tr > td:nth-child(5)').should('contain', "1,234.0");
         cy.get('span').contains('Reset Filters').click();
         cy.get('input[placeholder="Text Search"]').should('have.value', '');
     })
