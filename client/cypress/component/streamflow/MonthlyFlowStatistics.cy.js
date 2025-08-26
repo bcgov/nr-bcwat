9f3e738a7308ca729aa5a0a57d9cc843466ab8b3
Steven Sonvisen <sonvisenss@gmail.com>
2025-08-26 16:09:57 -0700

    fleshed-out tests for monthly flow statistics chart and brushing

--- client/cypress/component/streamflow/MonthlyFlowStatistics.cy.js
+++ client/cypress/component/streamflow/MonthlyFlowStatistics.cy.js
@@ -1,36 +1,39 @@
 import MonthlyFlowStatistics from "../../../src/components/streamflow/MonthlyFlowStatistics.vue";
-import flowDuration from '../../fixtures/flowDuration.json';
+import monthlyFlowStatistics from '../../fixtures/monthlyFlowStatistics.json';
 
-const chartData = flowDuration.monthlyFlowStatistics;
-
+const chartData = monthlyFlowStatistics;
+const axisElCount = 5;
+const elsPerBoxPlot = 6;
+const els = Object.keys(chartData).length * elsPerBoxPlot  + axisElCount;
 
 describe('<MonthlyFlowStatistics />', () => {
-    it.only('mounts and renders as expected', () => {
-        // cy.mount(MonthlyFlowStatistics, {
-        //     props: {
-        //         chartData,
-        //         startEndYears: [1914, 1922],
-        //         startEndMonths: ['Jan', 'Feb'],
-        //     }
-        // });
+    it('mounts and renders as expected', () => {
+        cy.mount(MonthlyFlowStatistics, {
+            props: {
+                data: chartData,
+                startEndYears: [1914, 1922],
+                startEndMonths: ['Jan', 'Feb'],
+            }
+        });
+        cy.get('.g-els').children().should('have.length', els);
     });
     it('brushes when user clicks and drags', () => {
         cy.mount(MonthlyFlowStatistics, {
             props: {
-                chartData,
+                data: chartData,
                 startEndYears: [1914, 1922],
                 startEndMonths: ['Jan', 'Feb'],
             }
-        })
+        });
 
         cy.window().then(win => {
             cy.get('g[data-cy="mfs-chart-brush"] > rect.overlay')
                 .trigger('mousedown', 10, 300, { which: 1, view: win })
                 .trigger('mousemove', 300, 300, { which: 1, view: win })
                 .trigger('mouseup', 300, 300, { which: 1, view: win })
-        })
+        });
 
         // roughly checks the width of the rect that's drawn
-        cy.get('rect.selection').should('have.css', 'width').and('contain', '286');
+        cy.get('rect.selection').should('have.css', 'width').and('contain', '291');
     });
 });
