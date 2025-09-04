53a8f732e6fa05bbfb09ec56f2b9c152e8cdc14a
Kashike-Umemura <kashikeumemura@gmail.com>
2025-09-04 15:35:39 -0700

    improved last test

--- airflow/etl_pipelines/tests/test_moe_hydrometric_historic.py
+++ airflow/etl_pipelines/tests/test_moe_hydrometric_historic.py
@@ -13,7 +13,7 @@ from etl_pipelines.tests.conftest import (
     MockDbConn
 )
 from freezegun import freeze_time
-from mock import patch
+from mock import patch, MagicMock, PropertyMock
 from callee import Contains
 import polars as pl
 import polars.testing as plt
@@ -214,6 +214,7 @@ def test_transform_data_discharge(
 @patch.object(QuarterlyMoeHydroHistoricPipeline, "_QuarterlyMoeHydroHistoricPipeline__get_all_source_urls")
 @patch("etl_pipelines.scrapers.QuarterlyPipeline.quarterly.moe_hydrometric_historic.logger")
 @patch("etl_pipelines.scrapers.QuarterlyPipeline.quarterly.moe_hydrometric_historic.pl.read_database")
+@freeze_time("2025-09-04 00:00:00 UTC")
 def test_transform_data_stage(
     fake_read_database,
     fake_logger,
@@ -290,6 +291,7 @@ def test_transform_data_stage(
 @patch.object(QuarterlyMoeHydroHistoricPipeline, "_QuarterlyMoeHydroHistoricPipeline__get_all_source_urls")
 @patch("etl_pipelines.scrapers.QuarterlyPipeline.quarterly.moe_hydrometric_historic.logger")
 @patch("etl_pipelines.scrapers.QuarterlyPipeline.quarterly.moe_hydrometric_historic.pl.read_database")
+@freeze_time("2025-09-04 00:00:00 UTC")
 def test_get_and_insert_new_station(
     fake_read_database,
     fake_logger,
@@ -417,11 +419,14 @@ def test_get_and_insert_new_station(
     fake_logger.info.assert_any_call(Contains("new stations. Checking if they are in BC"))
     fake_logger.info.assert_any_call("Finished getting new stations and inserting them into the database")
 
+@patch("etl_pipelines.scrapers.QuarterlyPipeline.quarterly.moe_hydrometric_historic.requests.get")
 @patch("etl_pipelines.scrapers.QuarterlyPipeline.quarterly.moe_hydrometric_historic.logger")
 @patch("etl_pipelines.scrapers.QuarterlyPipeline.quarterly.moe_hydrometric_historic.pl.read_database")
+@freeze_time("2025-09-04 00:00:00 UTC")
 def test_get_all_source_urls(
     fake_read_database,
     fake_logger,
+    fake_get,
 ):
     # This is an actual test that pings the internet. I know it's not isolated but I think it's the best way to test practical behaviour. Can be removed if not desired.
     # Set up fakes
@@ -433,11 +438,14 @@ def test_get_all_source_urls(
             "station_id": pl.Int64
         }
     )
+    with open("etl_pipelines/tests/test_constants/station_csv/moe_hydro_hist_url_page.txt", "r") as file:
+        html_txt = file.read()
+
+    fake_response = MagicMock()
+    fake_text = PropertyMock(return_value=html_txt)
+    fake_get.return_value = fake_response
+    type(fake_response).text = fake_text
 
     pipeline = QuarterlyMoeHydroHistoricPipeline(db_conn = MockDbConn(), date_now = pendulum.now("UTC"), archive_type="Stage")
 
     assert pipeline.source_url == {'Stage.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Stage.csv', 'Stage_Archive_2015Oct_2017Oct.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Stage_Archive_2015Oct_2017Oct.csv', 'Stage_Archive_2017Oct_2019Oct.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Stage_Archive_2017Oct_2019Oct.csv', 'Stage_Archive_2019Oct_2021Oct.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Stage_Archive_2019Oct_2021Oct.csv', 'Stage_Archive_2021Oct_2023Oct.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Stage_Archive_2021Oct_2023Oct.csv', 'Stage_Archive_Post_20231001.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Stage_Archive_Post_20231001.csv', 'Stage_Archive_Pre_20151001.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Stage_Archive_Pre_20151001.csv'}
-
-    pipeline = QuarterlyMoeHydroHistoricPipeline(db_conn = MockDbConn(), date_now = pendulum.now("UTC"), archive_type="Discharge")
-
-    assert pipeline.source_url == {'Discharge.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Discharge.csv', 'Discharge_Archive_2015Oct_2017Oct.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Discharge_Archive_2015Oct_2017Oct.csv', 'Discharge_Archive_2017Oct_2019Oct.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Discharge_Archive_2017Oct_2019Oct.csv', 'Discharge_Archive_2019Oct_2021Oct.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Discharge_Archive_2019Oct_2021Oct.csv', 'Discharge_Archive_2021Oct_2023Oct.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Discharge_Archive_2021Oct_2023Oct.csv', 'Discharge_Archive_Post_20231001.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Discharge_Archive_Post_20231001.csv', 'Discharge_Archive_Pre_20151001.csv': 'https://www.env.gov.bc.ca/wsd/data_searches/water/Discharge_Archive_Pre_20151001.csv'}
