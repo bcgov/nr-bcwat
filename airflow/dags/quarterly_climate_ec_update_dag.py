2159c4f60e27217fc43fd40bad423acf6ab6a40b
Kashike-Umemura <kashikeumemura@gmail.com>
2025-09-17 11:47:25 -0700

    added email default_args to dags that didn't have them

--- airflow/dags/quarterly_climate_ec_update_dag.py
+++ airflow/dags/quarterly_climate_ec_update_dag.py
@@ -8,13 +8,19 @@ executor_config_template = {
         "pod_template_file": "/opt/airflow/pod_templates/medium_task_template.yaml"
     }
 
+default_args = {
+    'email': ['technical@foundryspatial.com'],
+    'email_on_failure': True
+}
+
 @dag(
     dag_id="quarterly_ec_update_dag",
     # Cron for At 08:30 UTC (00:30 PST) on day-of-month 1 in every 3rd month.
     schedule_interval="30 8 1 */3 *",
     start_date=pendulum.datetime(2025, 6, 13, tz="UTC"),
     catchup=False,
-    tags=["climate", "quarterly"]
+    tags=["climate", "quarterly"],
+    default_args=default_args
 )
 def run_quarterly_ec_update_dag():
 
--- airflow/dags/quarterly_gw_moe_dag.py
+++ airflow/dags/quarterly_gw_moe_dag.py
@@ -8,13 +8,19 @@ executor_config_template = {
         "pod_template_file": "/opt/airflow/pod_templates/medium_task_template.yaml"
     }
 
+default_args = {
+    'email': ['technical@foundryspatial.com'],
+    'email_on_failure': True
+}
+
 @dag(
     dag_id="quarterly_moe_gw_update",
     # Cron for At 09:00 UTC (01:00 PST) on day-of-month 1 in every 3rd month.
     schedule_interval="0 9 1 */3 *",
     start_date=pendulum.datetime(2025, 6, 13, tz="UTC"),
     catchup=False,
-    tags=["groundwater", "quarterly"]
+    tags=["groundwater", "quarterly"],
+    default_args=default_args
 )
 def run_quarterly_gw_moe_update_dag():
 
