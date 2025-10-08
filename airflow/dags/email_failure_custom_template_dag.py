058ce1598f23cc85484b8729f947cc8e4030a02d
LiamF-FS <liam@foundryspatial.com>
2025-10-08 19:09:58 -0230

    add k8s back in as import, add timestamp as podAnnotation to trigger rebuild

--- airflow/dags/email_failure_custom_template_dag.py
+++ airflow/dags/email_failure_custom_template_dag.py
@@ -1,6 +1,8 @@
 import os
 import pendulum
 from airflow.decorators import dag, task
+from airflow.settings import AIRFLOW_HOME
+from kubernetes.client import models as k8s
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
