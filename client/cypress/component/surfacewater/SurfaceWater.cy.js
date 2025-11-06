23bdfd90bc724f067311083038ee44207698e1dc
Steven Sonvisen <sonvisenss@gmail.com>
2025-11-06 11:09:57 -0800

    updated tests

--- client/cypress/component/surfacewater/SurfaceWater.cy.js
+++ client/cypress/component/surfacewater/SurfaceWater.cy.js
@@ -1,4 +1,5 @@
-import SurfaceWater from "@/components/surfacewater/SurfaceWater.vue";
+import WaterPortal from "@/components/water-portal/WaterPortal.vue";
+import { portalHandler } from '@/utils/reactor.js';
 import surfaceWaterStations from '../../fixtures/surfaceWaterStations.json';
 
 const pointCount = surfaceWaterStations.features.length;
@@ -10,7 +11,12 @@ describe('<SurfaceWater />', () => {
         cy.intercept('**/station-statistics', {fixture: 'stationStatistics.json'});
     })
     it('mounts and loads main page contents', () => {
-        cy.mount(SurfaceWater);
+        cy.mount(WaterPortal, {
+            props: {
+                defaultViewType: 'surface'
+            }
+        });
+        portalHandler.updateViewType('surface');
         cy.get('.mapboxgl-canvas').should('exist').and('be.visible')
         // zoom out of the map, showing all points
         cy.wait(1000);
@@ -19,7 +25,12 @@ describe('<SurfaceWater />', () => {
         cy.get('.map-point-count > div > i').should('contain', pointCount);
     });
     it('mounts and loads report contents', () => {
-        cy.mount(SurfaceWater);
+        cy.mount(WaterPortal, {
+            props: {
+                defaultViewType: 'surface'
+            }
+        });
+        portalHandler.updateViewType('surface');
         cy.get('.q-virtual-scroll__content > .q-item:nth-child(1)').click();
         // details are displayed
         cy.get('.selected-point > pre:nth-child(1)').should('not.be.empty');
