ab6c8ad5b29cb92411600746b9ba6573967a083b
Kashike-Umemura <kashikeumemura@gmail.com>
2025-12-03 14:51:19 -0800

    minor update to the licence_bc_app_land table

---
+++ migrations/sql/V1.0.8__update_appurtenant_land.sql
@@ -0,0 +1,13 @@
+UPDATE
+    bcwat_lic.licence_bc_app_land
+SET
+    appurtenant_land = 'ALL LAND WITHIN BOUNDARIES OF VILLAGE OF MCBRIDE CARIBOO DISTRICT'
+WHERE
+    licence_no = '507984';
+
+UPDATE
+    bcwat_lic.licence_bc_app_land
+SET
+    appurtenant_land = 'Legal Description: 6122 CHEETAH RD DISTRICT LOT 101A, LILLOOET LAND DISTRICT PID: 002-889-684'
+WHERE
+    licence_no = '507659';
