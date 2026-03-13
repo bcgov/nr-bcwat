e0ff07a7cb789e3efde8fe632b183ef190aaa0ff
Nathan E <nathan@enerson.ca>
2026-03-13 10:32:14 -0600

    implemented endpoint

--- backend/tests/unit/test_utils.py
+++ backend/tests/unit/test_utils.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import json
+import io
+import zipfile
 
 def load_fixture(*subpath_parts: str) -> dict:
     """
@@ -11,3 +13,10 @@ def load_fixture(*subpath_parts: str) -> dict:
     fixture_path = base.joinpath(*subpath_parts).resolve()
     with fixture_path.open() as f:
         return json.load(f)
+
+def assert_zips_equal(data1, data2):
+    with zipfile.ZipFile(io.BytesIO(data1)) as z1, \
+         zipfile.ZipFile(io.BytesIO(data2)) as z2:
+        assert z1.namelist() == z2.namelist()
+        for name in z1.namelist():
+            assert z1.read(name) == z2.read(name), f"Mismatch in {name}"
