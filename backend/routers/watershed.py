1666674fdb14558995ec8a35696270797969a8ef
Steven Sonvisen <sonvisenss@gmail.com>
2026-03-19 16:16:14 -0700

    backend components

--- backend/routers/watershed.py
+++ backend/routers/watershed.py
@@ -10,6 +10,7 @@ from utils.watershed import (
 import json
 import polars as pl
 import polars_st as st
+import requests
 import shutil
 import os
 import zipfile
@@ -657,3 +658,43 @@ def get_watershed_polygon_as_file(id, format):
         })
 
     return response
+ 
+@watershed.route('/<int:id>/report/pdf', methods=['GET'])
+def get_watershed_report_pdf(id):
+    pdf_response = None
+
+    try:
+        payload = {
+            'lng': request.args.get('lng'),
+            'lat': request.args.get('lat'),
+            'wfi': id,
+            'watershedName': request.args.get('watershedName'),
+            'fwa': request.args.get('fwa'),
+            'title': request.args.get('title') or 'Watershed Summary',
+            'notes': request.args.get('notes') or '',
+            'userCustomization': request.args.get('userCustomization'),
+        }
+
+        pdf_response = requests.get(f"{os.getenv("PDF_CONVERTER_ENDPOINT")}/watershed/{id}/report/pdf?lng={payload['lng']}&lat={payload['lat']}&watershedName={payload['watershedName']}&fwa={payload['fwa']}&title={payload['title']}&notes={payload['notes']}&userCustomization={payload['userCustomization']}")
+        if pdf_response is None:
+            raise Exception({
+                "user_message": "Error generating the PDF. Please try again later",
+                "server_message": e,
+                "status_code": 500
+            })
+        
+        pdf_io = BytesIO(pdf_response.content)
+        pdf_io.seek(0)
+
+        return send_file(
+            pdf_io,
+            mimetype="application/pdf",
+            as_attachment=True,
+            download_name="test.zip"
+        ), 200
+    except Exception as e:
+        raise Exception({
+            "user_message": "Error generating the PDF. Please try again later",
+            "server_message": e,
+            "status_code": 500
+        })
