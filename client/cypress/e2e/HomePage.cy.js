1a2d3a5706f7e91dd173124de01a265f4126ca99
LiamF-FS <liam@foundryspatial.com>
2026-09-22 16:03:51 -0700

    Wait on navigate to/from HomePage

--- client/cypress/e2e/HomePage.cy.js
+++ client/cypress/e2e/HomePage.cy.js
@@ -1,19 +1,23 @@
 describe('HomePage', () => {
     it('links all navigate correctly', () => {
         cy.visit('/');
+        cy.wait(5000);
         cy.get('.home-page-links').children().should('have.length', 11);
         cy.get('.home-page-links > .q-item:nth-child(1)').click();
         cy.url().should('include', 'watershed');
         cy.get('.loader-container', { timeout: 30000 }).should('not.exist');
         cy.visit('/');
+        cy.wait(5000);
         cy.get('.home-page-links > .q-item:nth-child(3)').click();
         cy.url().should('include', 'streamflow');
         cy.get('.loader-container', { timeout: 30000 }).should('not.exist');
         cy.visit('/');
+        cy.wait(5000);
         cy.get('.home-page-links > .q-item:nth-child(5)').click();
         cy.url().should('include', 'surface-water');
         cy.get('.loader-container', { timeout: 30000 }).should('not.exist');
         cy.visit('/');
+        cy.wait(5000)
         cy.get('.home-page-links > .q-item:nth-child(7)').click();
         cy.url().should('include', 'groundwater/quality');
         cy.get('.loader-container', { timeout: 30000 }).should('not.exist');
